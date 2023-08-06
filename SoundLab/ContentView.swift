73b695bc89f4f96633064368c39b39795561c2fe
canacel3 <nishidakaede3@gmail.com>
2023-08-06 16:24:16 +0900

    first commit

---
+++ SoundLab/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SoundLab
+//
+//  Created by canacel on 2023/08/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SoundLab/SoundLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundLabApp.swift
+//  SoundLab
+//
+//  Created by canacel on 2023/08/06.
+//
+
+import SwiftUI
+
+@main
+struct SoundLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
