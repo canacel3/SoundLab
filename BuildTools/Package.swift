6cafdeb4d0c9bd6d1a242df97427073e3b6e1fbc
canacel3 <nishidakaede3@gmail.com>
2023-08-06 17:26:27 +0900

    SwiftLint 追加

---
+++ BuildTools/Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BuildTools",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "BuildTools",
+            targets: ["BuildTools"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/realm/SwiftLint.git", branch: "main")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "BuildTools",
+            dependencies: []),
+        .testTarget(
+            name: "BuildToolsTests",
+            dependencies: ["BuildTools"]),
+    ]
+)
